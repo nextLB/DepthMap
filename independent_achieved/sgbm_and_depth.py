2edc3cf1b931f1249f035adf752c28aa25f833c4
next <364574103@qq.com>
2026-01-23 11:56:46 +0800

    mine

--- independent_achieved/sgbm_and_depth.py
+++ independent_achieved/sgbm_and_depth.py
@@ -127,14 +127,18 @@ from Depth.depth_anything.dpt import DepthAnything
 from Depth.depth_anything.util.transform import Resize, NormalizeImage, PrepareForNet
 import cv2
 import torch
+import os
+from tqdm import tqdm
+import numpy as np
+import torch.nn.functional as F
 
 
 def detection_depth():
-    imagePath = ''
-    outDir = ''
+    imagePath = '/home/next_lb/桌面/next/depth_test_images/'
+    outDir = './output'
     encoder = 'vitl'
-    predOnly = ''
-    grayscale = ''
+    predOnly = None
+    grayscale = None
 
     marginWidth = 50
     captionHeight = 60
@@ -158,6 +162,70 @@ def detection_depth():
         NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         PrepareForNet(),
     ])
+
+    if os.path.isfile(imagePath):
+        if imagePath.endswith('txt'):
+            with open(imagePath, 'r') as f:
+                filenames = f.read().splitlines()
+        else:
+            filenames = [imagePath]
+    else:
+        filenames = os.listdir(imagePath)
+        filenames = [os.path.join(imagePath, filename) for filename in filenames if not filename.startswith('.')]
+        filenames.sort()
+
+    os.makedirs(outDir, exist_ok=True)
+
+    for filename in tqdm(filenames):
+        raw_image = cv2.imread(filename)
+        image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB) / 255.0
+
+        h, w = image.shape[:2]
+
+        image = transform({'image': image})['image']
+        image = torch.from_numpy(image).unsqueeze(0).to(DEVICE)
+
+        with torch.no_grad():
+            depth = depthAnything(image)
+
+        depth = F.interpolate(depth[None], (h, w), mode='bilinear', align_corners=False)[0, 0]
+        depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+
+        depth = depth.cpu().numpy().astype(np.uint8)
+
+        if grayscale:
+            depth = np.repeat(depth[..., np.newaxis], 3, axis=-1)
+        else:
+            depth = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
+
+        filename = os.path.basename(filename)
+
+        if predOnly:
+            cv2.imwrite(os.path.join(outDir, filename[:filename.rfind('.')] + '_depth.png'), depth)
+        else:
+            split_region = np.ones((raw_image.shape[0], marginWidth, 3), dtype=np.uint8) * 255
+            combined_results = cv2.hconcat([raw_image, split_region, depth])
+
+            caption_space = np.ones((captionHeight, combined_results.shape[1], 3), dtype=np.uint8) * 255
+            captions = ['Raw image', 'Depth Anything']
+            segment_width = w + marginWidth
+
+            for i, caption in enumerate(captions):
+                # Calculate text size
+                text_size = cv2.getTextSize(caption, font, fontScale, fontThickness)[0]
+
+                # Calculate x-coordinate to center the text
+                text_x = int((segment_width * i) + (w - text_size[0]) / 2)
+
+                # Add text caption
+                cv2.putText(caption_space, caption, (text_x, 40), font, fontScale, (0, 0, 0), fontThickness)
+
+            final_result = cv2.vconcat([caption_space, combined_results])
+
+            cv2.imwrite(os.path.join(outDir, filename[:filename.rfind('.')] + '_img_depth.png'), final_result)
+
+
+
 
 def main():
     detection_depth()
